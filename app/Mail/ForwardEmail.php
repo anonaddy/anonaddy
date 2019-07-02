111df04dcadaf0cbc192b3cbaa7ff082e554121a
Will Browning <will@willbrowning.me>
2019-07-02 11:25:59 +0100

    Updated list-unsubscribe

--- app/Mail/ForwardEmail.php
+++ app/Mail/ForwardEmail.php
@@ -54,7 +54,7 @@ class ForwardEmail extends Mailable implements ShouldQueue
         $replyToEmail = $this->alias->local_part.'+'.sha1(config('anonaddy.secret').$this->sender).'@'.$this->alias->domain;
 
         $email =  $this
-            ->from(config('mail.from.address'), $this->displayFrom."'".$this->sender."' via ".config('app.name'))
+            ->from(config('mail.from.address'), $this->displayFrom." '".$this->sender."' via ".config('app.name'))
             ->replyTo($replyToEmail, $this->sender)
             ->subject($this->emailSubject)
             ->text('emails.forward.text')->with([
@@ -68,8 +68,7 @@ class ForwardEmail extends Mailable implements ShouldQueue
             ])
             ->withSwiftMessage(function ($message) {
                 $message->getHeaders()
-                        ->addTextHeader('List-Unsubscribe', '<' . $this->deactivateUrl . '>,
-                        <mailto:' . $this->alias->id . '@unsubscribe.' . config('anonaddy.domain') . '>');
+                        ->addTextHeader('List-Unsubscribe', '<' . $this->deactivateUrl . '>, <mailto:' . $this->alias->id . '@unsubscribe.' . config('anonaddy.domain') . '>');
 
                 $message->getHeaders()
                         ->addTextHeader('Return-Path', 'bounce@' . config('anonaddy.domain'));
