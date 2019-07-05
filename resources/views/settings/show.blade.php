cb36a3aa01b9f679c897baf6f29392acd76ed2ea
Will Browning <will@willbrowning.me>
2019-07-05 18:48:25 +0100

    Updated rate limit message

--- resources/views/settings/show.blade.php
+++ resources/views/settings/show.blade.php
@@ -253,7 +253,7 @@
 
                     <div class="mt-4 w-24 border-b-2 border-grey-200"></div>
 
-                    <p class="mt-6">The from name is shown when you reply anonymously to a forwarded email. If set to empty then a default from name will be used for each alias e.g. "ebay{{ '@'.$user->username }}.anonaddy.me via AnonAddy".</p>
+                    <p class="mt-6">The from name is shown when you reply anonymously to a forwarded email. If set to empty then a default from name will be used for each alias e.g. "ebay{{ '@'.$user->username }}.{{ config('anonaddy.domain') }} via AnonAddy".</p>
 
                     <div class="mt-6 flex flex-wrap mb-4">
                         <label for="from_name" class="block text-grey-700 text-sm mb-2">
