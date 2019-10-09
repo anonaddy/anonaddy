c3f0451e5aa5c7d30d9156c318dec90149a5fdeb
Will Browning <will@willbrowning.me>
2019-10-09 15:17:55 +0100

    Fixed cors header

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,6 +18,6 @@ class Cors
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, X-Request-With');
+            ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, X-Requested-With');
     }
 }
