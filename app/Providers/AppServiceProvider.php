8b65ec58bef5211a909a539ad17fb67fbab2f839
Will <will@pop-os.localdomain>
2020-09-17 14:14:39 +0100

    Fix for double dot in URLs issue #36

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use Swift_Preferences;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Blade::withoutComponentTags();
+
+        Swift_Preferences::getInstance()->setQPDotEscape(true);
     }
 }
