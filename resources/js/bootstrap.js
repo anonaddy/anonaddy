76be5563342ab789d81502806d03875ddfce9537
Will Browning <will@willbrowning.me>
2022-10-11 17:25:58 +0100

    Fixed axios import

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,8 +4,8 @@
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require('axios')
-
+import axios from 'axios'
+window.axios = axios
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 
 window._ = require('lodash')
