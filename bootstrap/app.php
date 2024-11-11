17873a6fde5fe0fd875225a6253fd7d10136c674
Niels Bouma <9073152+nielscil@users.noreply.github.com>
2024-11-11 12:06:22 +0100

    more cleanup

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Session\Middleware\AuthenticateSession;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
