b465af46a92f9bad0f1d7d12f03e13951ca0fb52
12private12 <120792mon@gmail.com>
2025-08-14 23:42:40 +0800

    Delete resources directory

--- resources/views/emails/forward/text_banner.blade.php
+++
@@ -1,6 +0,0 @@
-<!--banner-info-->
-This email was sent to {{ $aliasEmail }}{{ $aliasDescription ? ' (' . $aliasDescription . ')' : '' }} from {{ $fromEmail }}{!! $replacedSubject !!}.
-To deactivate this alias copy and paste the url below into your web browser.
-
-{{ $deactivateUrl }}
-<!--banner-info-->
